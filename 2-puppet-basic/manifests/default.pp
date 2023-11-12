0a45cca941def0e432f98c69a3233c21b4a041fe
Jairo Andres Grajales Salinas <ingandresgrajales@gmail.com>
2023-11-12 11:44:48 -0500

    Change general structure

---
+++ 2-puppet-basic/manifests/default.pp
@@ -0,0 +1,14 @@
+$document_root = '/vagrant'
+include apache
+
+exec { 'Skip Message':
+  command => "echo ‘Este mensaje sólo se muestra si no se ha copiado el fichero index.html'",
+  unless => "test -f ${document_root}/index.html",
+  path => "/bin:/sbin:/usr/bin:/usr/sbin",
+}
+
+$ipv4_address = $facts['networking']['ip']
+notify { 'Showing machine Facts':
+  message => "Machine with ${::memory['system']['total']} of memory and ${::processorcount} processor/s.
+              Please check access to http://${ipv4_address}",
+}
